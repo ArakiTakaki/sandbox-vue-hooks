dec063e891a53402732d3f13131d2c6cc23f6eab
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-08-31 15:26:31 +0900

    🚀 : 初期化

---
+++ src/scripts/pages/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="index-component">
+      <div class="title">sample</div>
+      <div class="body">test</div>
+      <div class="footer">aiueo</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Prop, Component } from 'vue-property-decorator'
+
+@Component
+class Index extends Vue {
+  @Prop({ type: String })
+  readonly sample!: string
+}
+export default Index
+</script>
+
+<style lang="scss" scoped>
+//
+</style>
